4de1654f973147d1ccc4a520137641447a5d569d
alidegraeve <alexandra.degraeve@sps.edu>
2026-01-20 16:18:17 -0500

    Update Shooter.java

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,29 +1,36 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+//imports something
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//imports something
 
 public class Shooter extends SubsystemBase {
     private TalonFX _motor;
+    //creates motor object
 
-    public Indexer(int motorPort) {
+    public Shooter(int motorPort) {
         _motor = new TalonFX(motorPort);
     }
 
     public void spinFullSpeed() {
         _motor.set(1.0);
+    //tells motor to spin at full speed
     }
 
     public void spinFullSpeedInverted() {
         _motor.set(1.0);
+    //tells motor to spin at full speed in the opposite direction
     }
 
     public void stopMotor() {
         _motor.set(0.0);
+    //tells motor to stop spinning
     }
 
     public void setSpeed(int speed) {
         _motor.set(speed);
+    //tells motor to spin at a specific speed
     }
 }
