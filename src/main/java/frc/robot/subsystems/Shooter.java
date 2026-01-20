4b1de8471aab57caae733c8d4c6f2f0604a09213
Motoki Nakajima <motokinakajima0912@gmail.com>
2026-01-20 15:54:05 -0500

    added shooter (moch)

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+    private TalonFX _motor;
+
+    public Indexer(int motorPort) {
+        _motor = new TalonFX(motorPort);
+    }
+
+    public void spinFullSpeed() {
+        _motor.set(1.0);
+    }
+
+    public void spinFullSpeedInverted() {
+        _motor.set(1.0);
+    }
+
+    public void stopMotor() {
+        _motor.set(0.0);
+    }
+
+    public void setSpeed(int speed) {
+        _motor.set(speed);
+    }
+}
