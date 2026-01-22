6106839826a98206ca87be6621d523f7b0fda5b2
brighttweneboah-dot <bright.tweneboah@sps.edu>
2026-01-22 17:01:30 -0500

    Commented On SingleSwerveTest.java

--- src/main/java/frc/robot/commands/SingleSwerveTest.java
+++ src/main/java/frc/robot/commands/SingleSwerveTest.java
@@ -1,14 +1,17 @@
 package frc.robot.commands;
-
+// Angle of rotation for the robot's wheels.
 import edu.wpi.first.math.geometry.Rotation2d;
+// for human interface and command
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+//for reading joystick output
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.SwerveModule;
 
 public class SingleSwerveTest extends Command {
     SwerveModule _module;
-
+    //  Subsystem to controlmotors
+// Used for storing information
     public SingleSwerveTest(SwerveModule module) {
         _module = module;
         addRequirements(_module);
@@ -16,6 +19,7 @@ public class SingleSwerveTest extends Command {
     
     @Override
     public void execute() {
+        // Gains joystick values
         double vx = RobotContainer.driver.getLeftX();
         double vy = RobotContainer.driver.getLeftY();
 
@@ -25,7 +29,7 @@ public class SingleSwerveTest extends Command {
         Rotation2d angle = new Rotation2d(rot);
 
         SmartDashboard.putNumber("raw rot", angle.getRotations());
-
+// Converts the joystick values
         _module.setState(vel, angle);
-    }
+    }// Sets wheels speed and computed values
 }
